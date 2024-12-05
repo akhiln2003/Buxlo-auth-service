8bc2a76abaf0aac098203be3cd1d5edd6f262954
akhil <4khiln@gmail.com>
2024-12-05 19:43:58 +0530

    fix(esendOtpContolle): update error messages for better clarity

--- src/presentation/controller/user/resendOtpController.ts
+++ src/presentation/controller/user/resendOtpController.ts
@@ -20,10 +20,10 @@ export class ResendOtpController {
             await this.sendEmailServiceUseCase.execute({ email, name, otp })                // sending otp to email
             console.log("Your OTP is: "  , otp );
             
-            res.status(200).json({ message: "OTP  resent successfully", otp });
+            res.status(200).json({ message: "OTP  resent successfully please check you email" });
         } catch (err) {
             console.error("Error from resend Otp : ", err);
-            res.status(500).json({ message: "Invalid server error" });
+            res.status(500).json({ message: "Invalid server error try again later" });
         }
     }
 }
