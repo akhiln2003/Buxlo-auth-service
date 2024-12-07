d097b25b9c5784f87c4af50fc47a6f0c7a9282a6
akhil <4khiln@gmail.com>
2024-12-07 18:57:30 +0530

    style: format src/infrastructure/di/DIContainer.ts

--- src/infrastructure/di/DIContainer.ts
+++ src/infrastructure/di/DIContainer.ts
@@ -1,6 +1,9 @@
 import { IsendOtpEmailUseCase } from "../../application/interfaces/IemailService";
 import { IgetUser } from "../../application/interfaces/IgetUser";
-import { IotpVerification, IresendOtpUseCase } from "../../application/interfaces/Iotp";
+import {
+  IotpVerification,
+  IresendOtpUseCase,
+} from "../../application/interfaces/Iotp";
 import { IregisterUserTemporarily } from "../../application/interfaces/IregisterUserTemporarily";
 import { OTPService } from "../../application/services/OTPService";
 import { OtpVerification } from "../../application/usecases/user/otpVerification";
@@ -12,50 +15,47 @@ import { NodeMailerService } from "../external-services/nodeMailerService";
 import { RedisUserRepository } from "../repositories/cacheUserRepo";
 import { UserRepository } from "../repositories/userRepository";
 import { GetUserUseCase } from "../../application/usecases/user/getUser";
-
+import { IsignInUserUseCase } from "../../application/interfaces/IsignInUserUseCase";
+import { SignInUserUseCase } from "../../application/usecases/user/signInUser";
 
 class DIContainer {
-    private _authRepository: UserRepository;
-    private _rediseService: RedisUserRepository;
-    private _otpService: OTPService;
-    private _nodeMailerService: NodeMailerService;
-    private __jwtService: JwtService;
+  private _authRepository: UserRepository;
+  private _rediseService: RedisUserRepository;
+  private _otpService: OTPService;
+  private _nodeMailerService: NodeMailerService;
+  private __jwtService: JwtService;
 
-    constructor() {
-        this._authRepository = new UserRepository();
-        this._rediseService = new RedisUserRepository();
-        this._otpService = new OTPService();
-        this._nodeMailerService = new NodeMailerService();
-        this.__jwtService = new JwtService();
-    }
+  constructor() {
+    this._authRepository = new UserRepository();
+    this._rediseService = new RedisUserRepository();
+    this._otpService = new OTPService();
+    this._nodeMailerService = new NodeMailerService();
+    this.__jwtService = new JwtService();
+  }
 
-    
+  getUserUseCase(): IgetUser {
+    return new GetUserUseCase(this._authRepository);
+  }
 
-
-
-    getUserUseCase(): IgetUser{
-        return new GetUserUseCase(this._authRepository);
-    }
-
-    getTemporaryStoreUseCase():IregisterUserTemporarily {
-        return new RegisterUserTemporarily(this._rediseService, this._otpService);
-    }
-    getSendEmailServiceUseCase(): IsendOtpEmailUseCase {
-        return new SendOtpEmailUseCase(this._nodeMailerService);
-    }
-    getResendOtpUseCase(): IresendOtpUseCase{
-        return new ResendOtpUseCase(this._rediseService,this._otpService);
-    }
-    verifyUserUseCase(): IotpVerification{
-        return new OtpVerification(this._rediseService , this._authRepository , this.__jwtService);
-    }
-
-    
-    
-
+  getTemporaryStoreUseCase(): IregisterUserTemporarily {
+    return new RegisterUserTemporarily(this._rediseService, this._otpService);
+  }
+  getSendEmailServiceUseCase(): IsendOtpEmailUseCase {
+    return new SendOtpEmailUseCase(this._nodeMailerService);
+  }
+  getResendOtpUseCase(): IresendOtpUseCase {
+    return new ResendOtpUseCase(this._rediseService, this._otpService);
+  }
+  verifyUserUseCase(): IotpVerification {
+    return new OtpVerification(
+      this._rediseService,
+      this._authRepository,
+      this.__jwtService
+    );
+  }
+  signInUserUseCase(): IsignInUserUseCase {
+    return new SignInUserUseCase(this._authRepository, this.__jwtService);
+  }
 }
-
-
-
 
 export { DIContainer };
